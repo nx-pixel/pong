17e8e995f7d5dd25784df5b1c7014e0ac96172cb
nx-pixel <nuitxxxx@gmail.com>
2026-03-20 00:12:14 +0300

    Update run.bat to include model downloading
    Added model downloading step before starting the game.

--- run.bat
+++ run.bat
@@ -17,15 +17,15 @@ if errorlevel 1 (
     exit /b 1
 )
 
-:: [1/3] Проверка окружения
+:: [1/4] Проверка окружения
 if not exist %VENV_NAME% (
-    echo [1/3] First run: creating environment...
+    echo [1/4] First run: creating environment...
     python -m venv %VENV_NAME%
 ) else (
-    echo [1/3] Using existing environment...
+    echo [1/4] Using existing environment...
 )
 
-:: [2/3] Активация окружения
+:: [2/4] Активация окружения
 call %VENV_NAME%\Scripts\activate.bat
 
 :: Обновляем pip (исправленная команда)
@@ -36,8 +36,13 @@ python -m pip install --upgrade pip
 echo Installing dependencies...
 pip install requests tqdm pygame numpy
 
-:: [3/3] Запуск игры
-echo [2/3] Starting game...
+
+:: [3/4] Скачивание моделей
+echo [3/4] Скачивание моделей
+python download_models.py
+
+:: [4/4] Запуск игры
+echo [4/4] Starting game...
 python ping_pong_with_AI.py
 
 :: Деактивация
